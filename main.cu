d4e3ac544316c62e120cd3f3ff4ee2da0dd3306f
alexanderlvov <ytilituvovla@gmail.com>
2023-11-13 13:57:51 +0300

    Improvements.

--- main.cu
+++ main.cu
@@ -29,21 +29,22 @@ __global__ void kernel(const Aesi<512>* numberAndFactor, const uint64_t* const p
             bInc = gridDim.x,
             B_MAX = 2000000000U;
 
-    const Aesi n = numberAndFactor[0], factor = numberAndFactor[1];
+    const Aesi n = numberAndFactor[0]; Aesi* const factor = numberAndFactor + 1;
+
     const auto checkWriteRepeat = [&n](const Aesi<512> &value) {
-        if (value < 2 || value >= n) return false;
-        char buffer[512]{};
-        value.getString<10>(buffer, 512);
-        return printf("Found divisor: %s\n", buffer) > 14;
+        if (value < 2 || value >= n)
+            return false;
+        factor->atomicSet(value);
+        return true;
     };
-    if(!factor.isZero()) return;
+    if(!factor->isZero()) return;
 
     Aesi a = threadId * max_it + 2, e = 1;
     for (unsigned B = bStart; B < B_MAX; B += bInc) {
         auto primeUl = primes[0];
 
         for (unsigned pi = 0; primeUl < B; ++pi) {
-            if(!factor.isZero()) return;
+            if(!factor->isZero()) return;
             const unsigned power = log(static_cast<double>(B)) / log(static_cast<double>(primeUl));
             e *= static_cast<uint64_t>(pow(static_cast<double>(primeUl), static_cast<double>(power)));
             primeUl = primes[pi + 1];
@@ -52,7 +53,7 @@ __global__ void kernel(const Aesi<512>* numberAndFactor, const uint64_t* const p
         if (e == 1) continue;
 
         for (unsigned it = 0; it < max_it; ++it) {
-            if(!factor.isZero()) return;
+            if(!factor->isZero()) return;
             if (checkWriteRepeat(Aesi<512>::gcd(a, n)))
                 return;
 
@@ -68,8 +69,8 @@ int main(int argc, const char* const* const argv) {
     if(argc < 4)
         return std::printf("Usage: %s factorize <number> <primes location>", argv[0]);
 
-    thrust::device_vector<Aesi<512>> numberAndFactor = { { std::string_view(argv[2]) }, {} };
-    Timer::init() << "Factorizing number " << std::hex << std::showbase << numberAndFactor[0] << '.' << Timer::endl;
+    thrust::device_vector<Aesi<512>> numberAndFactor = { { std::string_view(argv[2]) }, { 0 } };
+    Timer::init() << "Factorizing number " << std::hex << std::showbase << numberAndFactor[0] << std::dec << '.' << Timer::endl;
 
     const thrust::device_vector<uint64_t> primes = loadPrimes(argv[3]);
     Timer::out << "Loaded prime table of " << primes.size() << " elements." << Timer::endl;
