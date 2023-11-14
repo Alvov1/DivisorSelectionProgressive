aee8986c0770d2f99f7fc1a675f27d5b66a983ef
alexanderlvov <ytilituvovla@gmail.com>
2023-11-15 00:04:11 +0300

    Improvements.

--- main.cu
+++ main.cu
@@ -32,7 +32,7 @@ __global__ void kernel(Aesi<512>* const numberAndFactor, const uint64_t* const p
     const Aesi<512> n = numberAndFactor[0]; Aesi<512>* const factor = numberAndFactor + 1;
 
     const auto checkFactor = [&n, &factor, &threadId] (const Aesi<512>& candidate) {
-        if(candidate >= n) return false;
+        if(candidate >= n || candidate < 2) return false;
 
         factor->atomicSet(candidate);
 
