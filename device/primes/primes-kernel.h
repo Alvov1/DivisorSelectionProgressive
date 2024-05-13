ec34d0fdb2fdcfd2b66bd0555e91060a34df0573
alexanderlvov <al.lvov777@gmail.com>
2024-05-13 15:07:49 +0300

    Small improvements in kernel.

--- device/primes/primes-kernel.h
+++ device/primes/primes-kernel.h
@@ -46,7 +46,7 @@ void kernel(Uns* const numberAndFactor, const unsigned* const primes, std::size_
             if(checkFactor(Uns::gcd(Uns::powm(a, e, n) - 1, n))) {
                 char buffer [256] {};
                 e.getString<10>(buffer, 256, true, false);
-                printf("Thread %u. Found factor for power %s.\n", threadId, buffer);
+                printf("Thread %u. Found factor for power %s, a = %llu.\n", threadId, buffer, a);
                 return;
             }
 
