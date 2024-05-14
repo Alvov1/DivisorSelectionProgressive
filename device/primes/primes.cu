e87547a61847048cfde3b1452a1cf507523fea1f
alexanderlvov <al.lvov777@gmail.com>
2024-05-14 23:26:15 +0300

    Revert "Improvements."
    This reverts commit 912b8bc70947b513b76d96cc4dfae154a5257118.

--- device/primes/primes.cu
+++ device/primes/primes.cu
@@ -35,7 +35,9 @@ int main(int argc, const char* const* const argv) {
     Timer::out << "Loaded table of primes with " << primes.size() << " elements." << Timer::endl;
 
     const auto threads = std::stoul(argv[3]), iterations = std::stoul(argv[4]);
-    Timer::out << std::chrono::zoned_time { std::chrono::current_zone(), std::chrono::system_clock::now() } << " Starting kernel <<<" << threads << ", " << threads << ">>>. Using bitness " << Uns::getBitness() << ". Iterations: " << iterations << Timer::endl;
+    const auto timePoint = std::chrono::system_clock::now();
+    const auto timeT = std::chrono::system_clock::to_time_t(timePoint);
+    Timer::out << std::ctime(&timeT) << " Starting kernel <<<" << threads << ", " << threads << ">>>. Using bitness " << Uns::getBitness() << ". Iterations: " << iterations << Timer::endl;
     kernel<<<threads, threads>>>(
             thrust::raw_pointer_cast(numberAndFactor.data()),
             thrust::raw_pointer_cast(primes.data()),
